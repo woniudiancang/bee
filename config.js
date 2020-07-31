5511f2920a7c2c113696fbd6311195f0b22f7ed9
woniu <2557188530@qq.com>
2020-07-31 23:28:01 +0800

    会员中心页面美化

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "2.0.2",
-  note: '全部订单页面优化',
+  version: "2.0.3",
+  note: '会员中心页面美化',
   subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
 }
