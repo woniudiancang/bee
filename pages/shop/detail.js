1b5e8af5b428008f7ba89f92853be405e56ffcf7
wangxy2020 <68831159+wangxy2020@users.noreply.github.com>
2020-08-04 20:01:34 +0800

    Add files via upload

--- pages/shop/detail.js
+++ pages/shop/detail.js
@@ -1,10 +1,17 @@
 // pages/shop/detail.js
+const WXAPI = require('apifm-wxapi')
+const AUTH = require('../../utils/auth')
+const APP = getApp()
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    markers: [],
+    
 
   },
 
@@ -12,6 +19,42 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var shopInfo = wx.getStorageSync('shopInfo') 
+    var marker = {
+      latitude: shopInfo.latitude,
+      longitude: shopInfo.longitude,
+      iconPath: "/images/marker.png",
+      height: 30,
+      width: 30,
+    }
+    var markers = new Array()  
+    markers.push(marker)     
+    this.setData({      
+      shopInfo,      
+      markers,
+    })
+  },
+  // 
+  phoneCall:function(){
+    var phoneNumber = this.data.shopInfo.linkPhone
+    wx.makePhoneCall({
+      phoneNumber,
+      // phoneNumber: phoneNumber,
+    })
+  },
+  // 
+  guideNow: function(){
+    var name = this.data.shopInfo.name
+    var address = this.data.shopInfo.address
+    var latitude = this.data.shopInfo.latitude
+    var longitude = this.data.shopInfo.longitude
+    wx.openLocation({
+      name: name,
+      address: address,
+      latitude: latitude,
+      longitude: longitude,
+    })
+
 
   },
 
@@ -26,6 +69,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    
 
   },
 
@@ -62,5 +106,6 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
 })
