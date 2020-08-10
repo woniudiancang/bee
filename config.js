4df97a07f1df83095b5fe639a95dbe43483b2ad0
woniu <2557188530@qq.com>
2020-08-10 09:52:13 +0800

    增加在线订座功能

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "2.2.0",
-  note: '增加优惠券功能支持',
+  version: "2.3.0",
+  note: '增加在线订座功能',
   subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
 }
---
+++ pages/booking/index.js
@@ -0,0 +1,112 @@
+const APP = getApp()
+const AUTH = require('../../utils/auth')
+const WXAPI = require('apifm-wxapi')
+
+// fixed首次打开不显示标题的bug
+APP.configLoadOK = () => {
+  
+}
+Page({
+  data: {
+    persionNum: ['1-2人', '3-4人', '5-8人', '8人以上'],
+    persionNumIndex: 0,
+    showDatetimePop: false,
+    formatter(type, value) {
+      if (type === 'year') {
+        return `${value}年`;
+      } else if (type === 'month') {
+        return `${value}月`;
+      } else if (type === 'day') {
+        return `${value}日`;
+      } else if (type === 'hour') {
+        return `${value}点`;
+      } else if (type === 'minute') {
+        return `${value}分`;
+      }
+      return value;
+    },
+    filter(type, options) {
+      if (type === 'minute') {
+        return options.filter((option) => option % 10 === 0);
+      }
+      return options;
+    },
+    currentDate: new Date().getTime(),
+    minDate: new Date().getTime(),
+  },
+  onLoad: function (options) {
+
+  },
+  onShow: function () {
+
+  },
+  changePersionNum(e) {
+    this.setData({
+      persionNumIndex: e.currentTarget.dataset.idx
+    })
+  },
+  async submit() {
+    if (!this.data.name) {
+      wx.showToast({
+        title: '请填写姓名',
+        icon: 'none'
+      })
+      return
+    }
+    if (!this.data.mobile) {
+      wx.showToast({
+        title: '请填写联系电话',
+        icon: 'none'
+      })
+      return
+    }
+    if (!this.data.time) {
+      wx.showToast({
+        title: '请选择到店时间',
+        icon: 'none'
+      })
+      return
+    }
+    const extJsonStr = {}
+    extJsonStr['姓名'] = this.data.name
+    extJsonStr['联系电话'] = this.data.mobile
+    extJsonStr['到店时间'] = this.data.time
+    extJsonStr['用餐人数'] = this.data.persionNum[this.data.persionNumIndex]
+    const res = await WXAPI.yuyueJoin({
+      token: wx.getStorageSync('token'),
+      yuyueId: wx.getStorageSync('zxdz'),
+      extJsonStr: JSON.stringify(extJsonStr)
+    })
+    if (res.code != 0) {
+      wx.showToast({
+        title: res.msg,
+        icon: 'none'
+      })
+    } else {
+      wx.showToast({
+        title: '提交成功',
+        icon: 'success'
+      })
+      setTimeout(() => {
+        wx.navigateBack()
+      }, 1000);
+    }
+  },
+  showDatetimePop() {
+    this.setData({
+      showDatetimePop: true
+    })
+  },
+  hideDatetimePop() {
+    this.setData({
+      showDatetimePop: false
+    })
+  },
+  confirm(e) {
+    const newDate = new Date(e.detail)
+    this.setData({
+      time: newDate.toLocaleString()
+    })
+    this.hideDatetimePop()
+  },
+})
