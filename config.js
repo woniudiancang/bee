1ac66951b4e1f992e95b7c7c4dad040e1c5c5f13
woniu <2557188530@qq.com>
2020-08-12 11:13:21 +0800

    增加每日签到功能

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "2.4.0",
-  note: '增加我的钱包，充值，充值记录，资金明细',
+  version: "2.5.0",
+  note: '增加每日签到功能',
   subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
 }
---
+++ pages/sign/index.js
@@ -0,0 +1,68 @@
+const WXAPI = require('apifm-wxapi')
+
+Page({
+  data: {
+    minDate: new Date().getTime(),
+    maxDate: new Date().getTime(),
+    formatter(day) {
+      return day;
+    },
+  },
+  onLoad: function(options) {
+    this.scoreSignLogs()
+  },
+  onShow: function() {
+  },
+  async scoreSignLogs() {
+    const res = await WXAPI.scoreSignLogs({
+      token: wx.getStorageSync('token')
+    })
+    if (res.code == 0) {
+      this.setData({
+        scoreSignLogs: res.data.result,
+        formatter(day) {
+          const _log = res.data.result.find(ele => {
+            const year = day.date.getYear() + 1900
+            let month = day.date.getMonth() + 1
+            month = month + ''
+            if (month.length == 1) {
+              month = '0' + month
+            }
+            let date = day.date.getDate() + ''
+            if (date.length == 1) {
+              date = '0' + date
+            }
+            return ele.dateAdd.indexOf(`${year}-${month}-${date}`) == 0
+          })
+          if (_log) {
+            day.bottomInfo = '已签到'
+          }
+          return day;
+        }
+      })
+    }
+  },
+  async sign() {
+    const res = await WXAPI.scoreSign(wx.getStorageSync('token'))
+    if (res.code == 10000) {
+      wx.showToast({
+        title: '签到成功',
+        icon: 'success'
+      })
+      this.scoreSignLogs()
+      return
+    }
+    if (res.code != 0) {
+      wx.showToast({
+        title: res.msg,
+        icon: 'none'
+      })
+    } else {
+      wx.showToast({
+        title: '签到成功',
+        icon: 'success'
+      })
+      this.scoreSignLogs()
+    }
+  },
+})
