cb21a57c4aa11c12ea9906f17380bdd35b3e0b1d
woniu <2557188530@qq.com>
2020-11-05 17:52:14 +0800

    图片使用缩略图

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "3.3.1",
-  note: 'fixed 营业时间样式 10:00-06:00 这种跨天的无法正确识别',
+  version: "3.3.2",
+  note: '图片使用缩略图',
   subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
 }
