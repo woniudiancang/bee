538a1d1cbe504400cb588fb4ec07289969d95ab8
woniudiancang <2557188530@qq.com>
2024-06-05 15:30:16 +0800

    fixed:未登录状态下，确认订单页面，合计显示未 underfined

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: "5.1.1",
-  note: 'fixed:繁体语言手机打开找不到语言包的bug',
+  version: "5.1.2",
+  note: 'fixed:未登录状态下，确认订单页面，合计显示未 underfined',
   subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
   customerServiceType: 'QW' // 客服类型，QW为企业微信，需要在后台系统参数配置企业ID和客服URL，否则为小程序的默认客服
 }
