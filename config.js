3262dac98d820a37ca222d8370794b86617e248c
woniudiancang <2557188530@qq.com>
2025-04-24 18:26:20 +0800

    我的礼品卡显示送礼人头像和昵称

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   version: "25.04.24",
-  note: '礼品卡赠送好友逻辑',
+  note: '我的礼品卡显示送礼人头像和昵称',
   subDomain: "beeorder", // 根据教程 https://www.it120.cc/help/qr6l4m.html 查看并设置你自己的 subDomain
   merchantId: 27, // 商户ID，可在后台工厂设置-->商户信息查看
   customerServiceType: 'QW' // 客服类型，QW为企业微信，需要在后台系统参数配置企业ID和客服URL，否则为小程序的默认客服
